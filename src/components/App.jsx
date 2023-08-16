6d4d5b425e051d4b4b9cbbd5874a8e562a6c0274
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2023-08-16 10:55:36 +0300

    first public

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'column',
+          alignItems: 'center',
+          fontSize: 20,
+          color: '#010101',
+        }}
+      ></div>
+    );
+  }
+}
